910a96362de47ed114d016ce50b43a99adc94ca5
jakubenglicky <jakubenglicky@gmail.com>
2021-05-26 21:20:42 +0200

    Nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,30 @@
+export default {
+    target: 'static',
+
+    head: {
+      title: 'ZORRAK - čistá práce',
+      htmlAttrs: {
+        lang: 'cs'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.png' },
+        { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'},
+        { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css'},
+      ],
+      script: [
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox-plus-jquery.min.js', body: true },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js', body: true },
+        { src: '/js/main.js', body: true }
+      ]
+    },
+
+    css: [
+      '~/assets/less/style'
+    ],
+  }
+  
