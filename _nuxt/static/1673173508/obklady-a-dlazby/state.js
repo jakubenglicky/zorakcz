c734792a0f125730e72e3440efa5ae843b13e0b7
jakubenglicky <jakubenglicky@users.noreply.github.com>
2023-01-08 10:25:14 +0000

    deploy: 3039e60904c801b07fc510fcb8248b2aaac83827

---
+++ _nuxt/static/1673173508/obklady-a-dlazby/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673173508",layout:"default",error:a,serverRendered:true,routePath:"\u002Fobklady-a-dlazby",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
